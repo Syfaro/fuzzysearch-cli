b30f1c777f7061c25ea7191175c07ae3a07f2959
Syfaro <syfaro@huefox.com>
2021-10-19 16:56:51 -0400

    Allow searching for matches with local dump.

--- src/main.rs
+++ src/main.rs
@@ -156,8 +156,11 @@ struct Opts {
     #[clap(short, long, default_value = "8")]
     concurrency: usize,
     /// FuzzySearch API key
+    #[clap(long, required_unless_present = "database-path")]
+    api_key: Option<String>,
+    /// FuzzySearch database dump path
     #[clap(long)]
-    api_key: String,
+    database_path: Option<String>,
     /// API limit per minute
     #[clap(short, long, default_value = "60")]
     limit: i32,
@@ -181,6 +184,70 @@ struct Opts {
 enum Action {
     AllSources,
     PerFile,
+}
+
+#[derive(Clone, Debug, serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "lowercase")]
+enum Site {
+    FurAffinity,
+    Weasyl,
+    E621,
+}
+
+#[derive(Clone, Debug, serde::Deserialize)]
+struct Item {
+    site: Site,
+    id: i64,
+    artists: String,
+    hash: Option<i64>,
+    posted_at: Option<String>,
+    updated_at: Option<String>,
+    #[serde(with = "b64_vec")]
+    sha256: Option<Vec<u8>>,
+    deleted: bool,
+}
+
+mod b64_vec {
+    use serde::Deserialize;
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Vec<u8>>, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        let val = <Option<String>>::deserialize(deserializer)?
+            .map(base64::decode)
+            .transpose()
+            .map_err(serde::de::Error::custom)?;
+
+        Ok(val)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub(crate) struct Node([u8; 8]);
+
+struct Hamming;
+
+impl bk_tree::Metric<Node> for Hamming {
+    fn distance(&self, a: &Node, b: &Node) -> u32 {
+        hamming::distance_fast(&a.0, &b.0).unwrap() as u32
+    }
+
+    fn threshold_distance(&self, a: &Node, b: &Node, _threshold: u32) -> Option<u32> {
+        Some(self.distance(a, b))
+    }
+}
+
+impl From<i64> for Node {
+    fn from(num: i64) -> Self {
+        Self(num.to_be_bytes())
+    }
+}
+
+impl From<Node> for i64 {
+    fn from(node: Node) -> Self {
+        i64::from_be_bytes(node.0)
+    }
 }
 
 #[tokio::main]
@@ -209,12 +276,14 @@ async fn main() {
         }
     }
 
+    info!("Creating working directories");
+
     let project_dir = directories::ProjectDirs::from("net", "fuzzysearch", "fuzzysearch-cli")
         .expect("Unable to get working directory");
     let data_dir = project_dir.data_dir();
     std::fs::create_dir_all(&data_dir).expect("Unable to create working directory");
 
-    let conn =
+    let mut conn =
         rusqlite::Connection::open(data_dir.join("hashes.db")).expect("Unable to create database");
 
     conn.execute(
@@ -223,9 +292,25 @@ async fn main() {
         hash INTEGER UNIQUE NOT NULL,
         data TEXT NOT NULL
     )",
-        rusqlite::params![],
+        [],
     )
     .expect("Unable to create hashes table");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS local_hashes (
+        id INTEGER PRIMARY KEY,
+        hash INTEGER NOT NULL,
+        site TEXT NOT NULL,
+        site_id INTEGER NOT NULL,
+        CONSTRAINT local_site UNIQUE(site, site_id)
+    )",
+        [],
+    )
+    .expect("Unable to create local_hashes table");
+    conn.execute(
+        "CREATE INDEX IF NOT EXISTS local_hash_idx ON local_hashes (hash)",
+        [],
+    )
+    .unwrap();
     conn.execute(
         "CREATE TABLE IF NOT EXISTS cache (
         id INTEGER PRIMARY KEY,
@@ -235,11 +320,11 @@ async fn main() {
         looked_up BOOL NOT NULL DEFAULT FALSE,
         FOREIGN KEY (hash_lookup_id) REFERENCES hashes(id)
     )",
-        rusqlite::params![],
+        [],
     )
     .expect("Unable to create cache table");
 
-    info!("Starting...");
+    info!("Starting to process files");
 
     let mut stmt = conn
         .prepare("SELECT 1 FROM cache WHERE path = ?1")
@@ -276,17 +361,22 @@ async fn main() {
             _ => continue,
         }
 
+        let path = path.to_string_lossy();
+
         if stmt
-            .exists(rusqlite::params![path.to_string_lossy()])
+            .exists(rusqlite::params![path])
             .expect("Unable to search cache for path")
         {
             debug!("{:?} already hashed", path);
         } else {
             insert_stmt
-                .execute(rusqlite::params![path.to_string_lossy()])
+                .execute(rusqlite::params![path])
                 .expect("Unable to insert item to cache");
         }
     }
+
+    drop(stmt);
+    drop(insert_stmt);
 
     let mut stmt = conn
         .prepare("SELECT id, path FROM cache WHERE hash IS NULL")
@@ -296,6 +386,7 @@ async fn main() {
         .expect("Unable to find items needing lookup")
         .filter_map(|row| row.ok())
         .collect();
+    drop(stmt);
 
     info!("Found {} files to evaluate", needed_hashes.len());
 
@@ -305,7 +396,7 @@ async fn main() {
     let pb = indicatif::ProgressBar::new(len as u64);
     pb.set_style(
         indicatif::ProgressStyle::default_bar()
-            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({eta})")
+            .template("{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta}) {msg}")
             .progress_chars("#>-"),
     );
 
@@ -339,116 +430,228 @@ async fn main() {
     let count: i32 = stmt
         .query_row([], |row| row.get(0))
         .expect("Unable to get count");
+    drop(stmt);
 
-    let mut stmt = conn
-        .prepare("SELECT id, hash FROM cache WHERE looked_up = FALSE LIMIT ?1")
-        .expect("Unable to prepare query to lookup items needing resolution");
+    let tree = if let Some(database_path) = opts.database_path.as_ref() {
+        let pb = indicatif::ProgressBar::new_spinner();
+        pb.set_style(
+            indicatif::ProgressStyle::default_bar()
+                .template("[{elapsed_precise}] {pos} completed, {per_sec} {msg}"),
+        );
+        pb.set_message("Indexing database");
 
-    let mut insert_stmt = conn
-        .prepare("INSERT OR IGNORE INTO hashes (hash, data) VALUES (?1, ?2)")
-        .expect("Unable to prepare query to insert hash info");
-    let mut update_stmt = conn
-        .prepare("UPDATE cache SET hash_lookup_id = ?1, looked_up = TRUE WHERE id = ?2")
-        .expect("Unable to prepare query to update cache item");
+        let path = Path::new(&database_path);
 
-    info!("Starting to look up hashes");
+        let file = tokio::fs::File::open(path)
+            .await
+            .expect("could not open database file");
+        let mut reader = csv_async::AsyncDeserializer::from_reader(file);
+        let mut records = reader.deserialize::<Item>();
 
-    let pb = indicatif::ProgressBar::new(count as u64);
-    pb.set_style(
-        indicatif::ProgressStyle::default_bar()
-            .template(
-                "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta})",
-            )
-            .progress_chars("#>-"),
-    );
-    pb.enable_steady_tick(100);
+        let tx = conn.transaction().unwrap();
 
-    loop {
-        let rows: Vec<(i32, i64)> = stmt
-            .query_map(rusqlite::params![&opts.limit], |row| {
-                Ok((row.get(0)?, row.get(1)?))
-            })
-            .expect("Unable to lookup items needing resolution")
-            .filter_map(|row| row.ok())
-            .collect();
+        let mut stmt = tx
+            .prepare("INSERT OR IGNORE INTO local_hashes (hash, site, site_id) VALUES (?1, ?2, ?3)")
+            .unwrap();
 
-        if rows.is_empty() {
-            break;
-        }
+        let mut tree = bk_tree::BKTree::new(Hamming);
 
-        let start = std::time::Instant::now();
+        while let Some(Ok(row)) = records.next().await {
+            pb.inc(1);
 
-        let mut lookup = std::collections::HashMap::new();
-        for row in rows.iter() {
-            lookup.insert(row.1, row.0);
-        }
+            if let Some(hash) = row.hash {
+                stmt.execute(rusqlite::params![
+                    hash,
+                    serde_json::to_string(&row.site).unwrap(),
+                    row.id
+                ])
+                .expect("could not insert local hash");
 
-        for chunk in rows.chunks(10) {
-            let items: Vec<_> = chunk.iter().map(|chunk| chunk.1).collect();
-            let hashes = loop {
-                match get_hashes(&opts.api_key, &items).await {
-                    Ok(hashes) => break hashes,
-                    Err(err) => {
-                        warn!("Got API error, retrying in 30 seconds: {}", err);
-                        tokio::time::sleep(std::time::Duration::from_secs(30)).await;
-                    }
-                }
-            };
-            for item in chunk.iter() {
-                if let Some(files) = hashes.get(&item.1) {
-                    let data =
-                        serde_json::to_string(&files).expect("Unable to serialize hash lookup");
-                    insert_stmt
-                        .execute(rusqlite::params![item.1, &data])
-                        .expect("Unable to insert hash lookup data");
-                    let hash_id = conn.last_insert_rowid();
-                    update_stmt
-                        .execute(rusqlite::params![hash_id, item.0])
-                        .expect("Unable to update cache with hash data ID");
-                } else {
-                    update_stmt
-                        .execute(rusqlite::params![rusqlite::types::Null, item.0])
-                        .expect("Unable to set hash lookup to None");
+                let hash: Node = hash.into();
+                if tree.find_exact(&hash).is_none() {
+                    tree.add(hash);
                 }
             }
-            pb.inc(chunk.len() as u64);
         }
 
-        if rows.len() < 30 {
-            break;
+        drop(stmt);
+        tx.commit().unwrap();
+
+        pb.finish_at_current_pos();
+
+        Some(tree)
+    } else if let Some(api_key) = opts.api_key {
+        let mut stmt = conn
+            .prepare("SELECT id, hash FROM cache WHERE looked_up = FALSE LIMIT ?1")
+            .expect("Unable to prepare query to lookup items needing resolution");
+
+        let mut insert_stmt = conn
+            .prepare("INSERT OR IGNORE INTO hashes (hash, data) VALUES (?1, ?2)")
+            .expect("Unable to prepare query to insert hash info");
+        let mut update_stmt = conn
+            .prepare("UPDATE cache SET hash_lookup_id = ?1, looked_up = TRUE WHERE id = ?2")
+            .expect("Unable to prepare query to update cache item");
+
+        let pb = indicatif::ProgressBar::new(count as u64);
+        pb.set_style(
+            indicatif::ProgressStyle::default_bar()
+                .template(
+                    "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] {pos}/{len} ({eta}): {msg}"
+                )
+                .progress_chars("#>-")
+        );
+        pb.enable_steady_tick(100);
+        pb.set_message("Looking up hashes");
+
+        loop {
+            let rows: Vec<(i32, i64)> = stmt
+                .query_map(rusqlite::params![&opts.limit], |row| {
+                    Ok((row.get(0)?, row.get(1)?))
+                })
+                .expect("Unable to lookup items needing resolution")
+                .filter_map(|row| row.ok())
+                .collect();
+
+            if rows.is_empty() {
+                break;
+            }
+
+            let start = std::time::Instant::now();
+
+            let mut lookup = std::collections::HashMap::new();
+            for row in rows.iter() {
+                lookup.insert(row.1, row.0);
+            }
+
+            for chunk in rows.chunks(10) {
+                let items: Vec<_> = chunk.iter().map(|chunk| chunk.1).collect();
+                let hashes = loop {
+                    match get_hashes(&api_key, &items).await {
+                        Ok(hashes) => break hashes,
+                        Err(err) => {
+                            warn!("Got API error, retrying in 30 seconds: {}", err);
+                            tokio::time::sleep(std::time::Duration::from_secs(30)).await;
+                        }
+                    }
+                };
+                for item in chunk.iter() {
+                    if let Some(files) = hashes.get(&item.1) {
+                        let data =
+                            serde_json::to_string(&files).expect("Unable to serialize hash lookup");
+                        insert_stmt
+                            .execute(rusqlite::params![item.1, &data])
+                            .expect("Unable to insert hash lookup data");
+                        let hash_id = conn.last_insert_rowid();
+                        update_stmt
+                            .execute(rusqlite::params![hash_id, item.0])
+                            .expect("Unable to update cache with hash data ID");
+                    } else {
+                        update_stmt
+                            .execute(rusqlite::params![rusqlite::types::Null, item.0])
+                            .expect("Unable to set hash lookup to None");
+                    }
+                }
+                pb.inc(chunk.len() as u64);
+            }
+
+            if rows.len() < 30 {
+                break;
+            }
+
+            let delay: i32 = 61 - (start.elapsed().as_secs() as i32);
+            if delay > 0 {
+                tokio::time::sleep(std::time::Duration::from_secs(delay as u64)).await;
+            }
         }
 
-        let delay: i32 = 61 - (start.elapsed().as_secs() as i32);
-        if delay > 0 {
-            tokio::time::sleep(std::time::Duration::from_secs(delay as u64)).await;
-        }
-    }
+        pb.finish_with_message("Completed hash lookup");
 
-    pb.finish_with_message("Completed hash lookup");
+        None
+    } else {
+        None
+    };
 
     let mut f = tokio::fs::File::create(opts.output)
         .await
         .expect("Unable to create output file");
 
+    let mut stmt = conn
+        .prepare("SELECT cache.hash FROM cache WHERE cache.path = ?1")
+        .unwrap();
+
+    info!("Completed index, evaluating files");
+
+    let files = walkdir::WalkDir::new(&opts.directory)
+        .into_iter()
+        .filter_map(|entry| entry.ok())
+        .filter_map(|entry| {
+            let path = entry.path().to_string_lossy();
+
+            stmt.query_map(&[&path], |row| row.get::<_, i64>(0))
+                .expect("could not query rows")
+                .find_map(|row| row.ok())
+                .map(|hash| (path.to_string(), hash))
+        });
+
+    let mut sources: HashMap<String, Vec<String>> = HashMap::new();
+
+    let mut stmt = conn
+        .prepare("SELECT data FROM hashes WHERE hash = ?1")
+        .expect("Unable to select hash data");
+    let mut tree_stmt = conn
+        .prepare("SELECT site, site_id FROM local_hashes WHERE hash = ?1")
+        .unwrap();
+
+    info!("Calculating image sources");
+
+    for (path, hash) in files {
+        let entry = sources.entry(path).or_default();
+
+        let matches: Vec<String> = if let Some(tree) = &tree {
+            let mut matches = Vec::new();
+
+            for (_distance, matching_hash) in tree.find(&hash.into(), 3).into_iter() {
+                tree_stmt
+                    .query_map([i64::from(*matching_hash)], |row| {
+                        Ok((row.get::<_, String>(0)?, row.get::<_, i64>(1)?))
+                    })
+                    .unwrap()
+                    .into_iter()
+                    .filter_map(|row| row.ok())
+                    .for_each(|(site, site_id)| {
+                        let site: Site = serde_json::from_str(&site).unwrap();
+                        matches.push(url_for_tuple(site, site_id));
+                    });
+            }
+
+            matches
+        } else {
+            let mut matches = Vec::new();
+
+            for data in stmt
+                .query_map([hash], |row| row.get::<_, String>(0))
+                .unwrap()
+            {
+                let data = data.unwrap();
+                let files = serde_json::from_str::<Vec<File>>(&data).unwrap_or_default();
+                matches.extend(files.into_iter().map(url_for_file));
+            }
+
+            matches
+        };
+
+        *entry = matches;
+    }
+
+    info!("Sources calculated, writing output");
+
     match opts.action {
         Action::AllSources => {
-            let mut stmt = conn
-                .prepare("SELECT data FROM hashes")
-                .expect("Unable to select hash data");
-
-            let sources_data = stmt
-                .query_map([], |row| row.get::<_, String>(0))
-                .expect("Unable to find items needing lookup");
-
-            let sources = sources_data.into_iter().map(|item| {
-                let s = item.expect("Row was bad");
-                let data: Vec<File> =
-                    serde_json::from_str(&s).expect("Database contained malformed data");
-
-                data.into_iter().map(url_for_file).collect::<Vec<String>>()
-            });
-
-            let mut sources: Vec<String> = sources.into_iter().flatten().collect();
+            let mut sources: Vec<String> = sources
+                .into_iter()
+                .map(|(_path, sources)| sources)
+                .flatten()
+                .collect();
             sources.sort();
             sources.dedup();
 
@@ -459,37 +662,12 @@ async fn main() {
             }
         }
         Action::PerFile => {
-            let mut stmt = conn
-                .prepare("SELECT cache.path, hashes.data FROM cache JOIN hashes ON hashes.id = cache.hash_lookup_id WHERE cache.path = ?1")
-                .expect("Unable to prepare cache lookup query");
-
             let mut csv = csv_async::AsyncWriter::from_writer(f);
 
-            for entry in walkdir::WalkDir::new(opts.directory)
-                .into_iter()
-                .filter_map(|entry| entry.ok())
-            {
-                let path = entry.path().to_string_lossy();
-
-                let items = stmt
-                    .query_map([&path], |row| {
-                        Ok((row.get::<_, String>(0)?, row.get::<_, String>(1)?))
-                    })
-                    .expect("Unable to query")
-                    .filter_map(|data| data.ok())
-                    .filter_map(|(path, data)| {
-                        serde_json::from_str::<Vec<File>>(&data)
-                            .map(|data| (path, data))
-                            .ok()
-                    });
-
-                for (path, data) in items {
-                    let data: Vec<String> = data.into_iter().map(url_for_file).collect();
-
-                    csv.write_record(&[path, data.join(" ")])
-                        .await
-                        .expect("could not write csv record");
-                }
+            for (path, sources) in sources {
+                csv.write_record(&[path, sources.join(" ")])
+                    .await
+                    .expect("could not write csv record");
             }
         }
     }
@@ -551,11 +729,19 @@ fn url_for_file(item: File) -> String {
         }
         SiteInfo::Twitter => format!(
             "https://twitter.com/{}/status/{}",
-            item.artists.unwrap().get(0).unwrap(),
+            item.artists.unwrap()[0],
             item.site_id
         ),
         SiteInfo::Weasyl => {
             format!("https://www.weasyl.com/view/{}/", item.site_id)
         }
+    }
+}
+
+fn url_for_tuple(site: Site, site_id: i64) -> String {
+    match site {
+        Site::FurAffinity => format!("https://www.furaffinity.net/view/{}/", site_id),
+        Site::E621 => format!("https://e621.net/post/show/{}", site_id),
+        Site::Weasyl => format!("https://www.weasyl.com/view/{}", site_id),
     }
 }
